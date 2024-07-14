abc65f2ea3840ac03c12618d9e2c8d0ac12ec958
Sakii <13037299+SakiiCode@users.noreply.github.com>
2024-07-14 20:14:52 +0200

    simulation

---
+++ src/asa159.rs
@@ -0,0 +1,395 @@
+#![allow(
+    dead_code,
+    mutable_transmutes,
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
+    unused_assignments,
+    unused_mut
+)]
+extern "C" {
+    fn malloc(_: libc::c_ulong) -> *mut libc::c_void;
+    fn free(_: *mut libc::c_void);
+    fn printf(_: *const libc::c_char, _: ...) -> libc::c_int;
+    fn exp(_: libc::c_double) -> libc::c_double;
+    fn log(_: libc::c_double) -> libc::c_double;
+}
+#[no_mangle]
+pub unsafe extern "C" fn i4_max(mut i1: libc::c_int, mut i2: libc::c_int) -> libc::c_int {
+    let mut value: libc::c_int = 0;
+    if i2 < i1 {
+        value = i1;
+    } else {
+        value = i2;
+    }
+    return value;
+}
+#[no_mangle]
+pub unsafe extern "C" fn i4_min(mut i1: libc::c_int, mut i2: libc::c_int) -> libc::c_int {
+    let mut value: libc::c_int = 0;
+    if i1 < i2 {
+        value = i1;
+    } else {
+        value = i2;
+    }
+    return value;
+}
+#[no_mangle]
+pub unsafe extern "C" fn i4mat_print(
+    mut m: libc::c_int,
+    mut n: libc::c_int,
+    mut a: *mut libc::c_int,
+    mut title: *mut libc::c_char,
+) {
+    i4mat_print_some(m, n, a, 1 as libc::c_int, 1 as libc::c_int, m, n, title);
+}
+#[no_mangle]
+pub unsafe extern "C" fn i4mat_print_some(
+    mut m: libc::c_int,
+    mut n: libc::c_int,
+    mut a: *mut libc::c_int,
+    mut ilo: libc::c_int,
+    mut jlo: libc::c_int,
+    mut ihi: libc::c_int,
+    mut jhi: libc::c_int,
+    mut title: *mut libc::c_char,
+) {
+    let mut i: libc::c_int = 0;
+    let mut i2hi: libc::c_int = 0;
+    let mut i2lo: libc::c_int = 0;
+    let mut j: libc::c_int = 0;
+    let mut j2hi: libc::c_int = 0;
+    let mut j2lo: libc::c_int = 0;
+    printf(b"\n\0" as *const u8 as *const libc::c_char);
+    printf(b"%s\n\0" as *const u8 as *const libc::c_char, title);
+    if m <= 0 as libc::c_int || n <= 0 as libc::c_int {
+        printf(b"\n\0" as *const u8 as *const libc::c_char);
+        printf(b"  (None)\n\0" as *const u8 as *const libc::c_char);
+        return;
+    }
+    j2lo = jlo;
+    while j2lo <= jhi {
+        j2hi = j2lo + 10 as libc::c_int - 1 as libc::c_int;
+        j2hi = i4_min(j2hi, n);
+        j2hi = i4_min(j2hi, jhi);
+        printf(b"\n\0" as *const u8 as *const libc::c_char);
+        printf(b"  Col:\0" as *const u8 as *const libc::c_char);
+        j = j2lo;
+        while j <= j2hi {
+            printf(
+                b"  %6d\0" as *const u8 as *const libc::c_char,
+                j - 1 as libc::c_int,
+            );
+            j += 1;
+        }
+        printf(b"\n\0" as *const u8 as *const libc::c_char);
+        printf(b"  Row\n\0" as *const u8 as *const libc::c_char);
+        printf(b"\n\0" as *const u8 as *const libc::c_char);
+        i2lo = i4_max(ilo, 1 as libc::c_int);
+        i2hi = i4_min(ihi, m);
+        i = i2lo;
+        while i <= i2hi {
+            printf(
+                b"%5d:\0" as *const u8 as *const libc::c_char,
+                i - 1 as libc::c_int,
+            );
+            j = j2lo;
+            while j <= j2hi {
+                printf(
+                    b"  %6d\0" as *const u8 as *const libc::c_char,
+                    *a.offset((i - 1 as libc::c_int + (j - 1 as libc::c_int) * m) as isize),
+                );
+                j += 1;
+            }
+            printf(b"\n\0" as *const u8 as *const libc::c_char);
+            i += 1;
+        }
+        j2lo = j2lo + 10 as libc::c_int;
+    }
+}
+#[no_mangle]
+pub unsafe extern "C" fn i4vec_print(
+    mut n: libc::c_int,
+    mut a: *mut libc::c_int,
+    mut title: *mut libc::c_char,
+) {
+    let mut i: libc::c_int = 0;
+    printf(b"\n\0" as *const u8 as *const libc::c_char);
+    printf(b"%s\n\0" as *const u8 as *const libc::c_char, title);
+    printf(b"\n\0" as *const u8 as *const libc::c_char);
+    i = 0 as libc::c_int;
+    while i < n {
+        printf(
+            b"  %6d: %8d\n\0" as *const u8 as *const libc::c_char,
+            i,
+            *a.offset(i as isize),
+        );
+        i += 1;
+    }
+}
+#[no_mangle]
+pub unsafe extern "C" fn i4vec_sum(mut n: libc::c_int, mut a: *mut libc::c_int) -> libc::c_int {
+    let mut i: libc::c_int = 0;
+    let mut sum: libc::c_int = 0;
+    sum = 0 as libc::c_int;
+    i = 0 as libc::c_int;
+    while i < n {
+        sum = sum + *a.offset(i as isize);
+        i += 1;
+    }
+    return sum;
+}
+#[no_mangle]
+pub unsafe extern "C" fn r8_uniform_01(mut seed: *mut libc::c_int) -> libc::c_double {
+    let mut k: libc::c_int = 0;
+    let mut r: libc::c_double = 0.;
+    k = *seed / 127773 as libc::c_int;
+    *seed = 16807 as libc::c_int * (*seed - k * 127773 as libc::c_int) - k * 2836 as libc::c_int;
+    if *seed < 0 as libc::c_int {
+        *seed = *seed + 2147483647 as libc::c_int;
+    }
+    r = *seed as libc::c_double * 4.656612875E-10f64;
+    return r;
+}
+#[no_mangle]
+pub unsafe extern "C" fn rcont2(
+    mut nrow: libc::c_int,
+    mut ncol: libc::c_int,
+    mut nrowt: *mut libc::c_int,
+    mut ncolt: *mut libc::c_int,
+    mut key: *mut libc::c_int,
+    mut seed: *mut libc::c_int,
+    mut matrix: *mut libc::c_int,
+    mut ierror: *mut libc::c_int,
+) {
+    let mut done1: libc::c_int = 0;
+    let mut done2: libc::c_int = 0;
+    let mut fact: *mut libc::c_double = 0 as *const libc::c_double as *mut libc::c_double;
+    let mut i: libc::c_int = 0;
+    let mut ia: libc::c_int = 0;
+    let mut iap: libc::c_int = 0;
+    let mut ib: libc::c_int = 0;
+    let mut ic: libc::c_int = 0;
+    let mut id: libc::c_int = 0;
+    let mut idp: libc::c_int = 0;
+    let mut ie: libc::c_int = 0;
+    let mut igp: libc::c_int = 0;
+    let mut ihp: libc::c_int = 0;
+    let mut ii: libc::c_int = 0;
+    let mut iip: libc::c_int = 0;
+    let mut j: libc::c_int = 0;
+    let mut jc: libc::c_int = 0;
+    let mut jwork: *mut libc::c_int = 0 as *mut libc::c_int;
+    let mut l: libc::c_int = 0;
+    let mut lsm: libc::c_int = 0;
+    let mut lsp: libc::c_int = 0;
+    let mut m: libc::c_int = 0;
+    let mut nll: libc::c_int = 0;
+    let mut nlm: libc::c_int = 0;
+    let mut nlmp: libc::c_int = 0;
+    let mut nrowtl: libc::c_int = 0;
+    let mut ntotal: libc::c_int = 0 as libc::c_int;
+    let mut r: libc::c_double = 0.;
+    let mut sumprb: libc::c_double = 0.;
+    let mut x: libc::c_double = 0.;
+    let mut y: libc::c_double = 0.;
+    *ierror = 0 as libc::c_int;
+    if *key == 0 {
+        *key = 1 as libc::c_int;
+        if nrow <= 1 as libc::c_int {
+            printf(b"\n\0" as *const u8 as *const libc::c_char);
+            printf(b"RCONT - Fatal error!\n\0" as *const u8 as *const libc::c_char);
+            printf(
+                b"  Input number of rows is less than 2.\n\0" as *const u8 as *const libc::c_char,
+            );
+            *ierror = 1 as libc::c_int;
+            return;
+        }
+        if ncol <= 1 as libc::c_int {
+            printf(b"\n\0" as *const u8 as *const libc::c_char);
+            printf(b"RCONT - Fatal error!\n\0" as *const u8 as *const libc::c_char);
+            printf(
+                b"  The number of columns is less than 2.\n\0" as *const u8 as *const libc::c_char,
+            );
+            *ierror = 2 as libc::c_int;
+            return;
+        }
+        i = 0 as libc::c_int;
+        while i < nrow {
+            if *nrowt.offset(i as isize) <= 0 as libc::c_int {
+                printf(b"\n\0" as *const u8 as *const libc::c_char);
+                printf(b"RCONT - Fatal error!\n\0" as *const u8 as *const libc::c_char);
+                printf(
+                    b"  An entry in the row sum vector is not positive.\n\0" as *const u8
+                        as *const libc::c_char,
+                );
+                *ierror = 3 as libc::c_int;
+                return;
+            }
+            i += 1;
+        }
+        j = 0 as libc::c_int;
+        while j < ncol {
+            if *ncolt.offset(j as isize) <= 0 as libc::c_int {
+                printf(b"\n\0" as *const u8 as *const libc::c_char);
+                printf(b"RCONT - Fatal error!\n\0" as *const u8 as *const libc::c_char);
+                printf(
+                    b"  An entry in the column sum vector is not positive.\n\0" as *const u8
+                        as *const libc::c_char,
+                );
+                *ierror = 4 as libc::c_int;
+                return;
+            }
+            j += 1;
+        }
+        if i4vec_sum(ncol, ncolt) != i4vec_sum(nrow, nrowt) {
+            printf(b"\n\0" as *const u8 as *const libc::c_char);
+            printf(b"RCONT - Fatal error!\n\0" as *const u8 as *const libc::c_char);
+            printf(
+                b"  The row and column sum vectors do not have the same sum.\n\0" as *const u8
+                    as *const libc::c_char,
+            );
+            *ierror = 6 as libc::c_int;
+            return;
+        }
+        ntotal = i4vec_sum(ncol, ncolt);
+        if !fact.is_null() {
+            free(fact as *mut libc::c_void);
+        }
+        fact = malloc(
+            ((ntotal + 1 as libc::c_int) as libc::c_ulong)
+                .wrapping_mul(::core::mem::size_of::<libc::c_double>() as libc::c_ulong),
+        ) as *mut libc::c_double;
+        x = 0.0f64;
+        *fact.offset(0 as libc::c_int as isize) = 0.0f64;
+        i = 1 as libc::c_int;
+        while i <= ntotal {
+            x = x + log(i as libc::c_double);
+            *fact.offset(i as isize) = x;
+            i += 1;
+        }
+    }
+    jwork = malloc(
+        (ncol as libc::c_ulong)
+            .wrapping_mul(::core::mem::size_of::<libc::c_int>() as libc::c_ulong),
+    ) as *mut libc::c_int;
+    i = 0 as libc::c_int;
+    while i < ncol - 1 as libc::c_int {
+        *jwork.offset(i as isize) = *ncolt.offset(i as isize);
+        i += 1;
+    }
+    jc = ntotal;
+    l = 0 as libc::c_int;
+    while l < nrow - 1 as libc::c_int {
+        nrowtl = *nrowt.offset(l as isize);
+        ia = nrowtl;
+        ic = jc;
+        jc = jc - nrowtl;
+        m = 0 as libc::c_int;
+        while m < ncol - 1 as libc::c_int {
+            id = *jwork.offset(m as isize);
+            ie = ic;
+            ic = ic - id;
+            ib = ie - ia;
+            ii = ib - id;
+            if ie == 0 as libc::c_int {
+                ia = 0 as libc::c_int;
+                j = m;
+                while j < ncol {
+                    *matrix.offset((l + j * nrow) as isize) = 0 as libc::c_int;
+                    j += 1;
+                }
+                break;
+            } else {
+                r = r8_uniform_01(seed);
+                done1 = 0 as libc::c_int;
+                loop {
+                    nlm = ((ia * id) as libc::c_double / ie as libc::c_double + 0.5f64)
+                        as libc::c_int;
+                    iap = ia + 1 as libc::c_int;
+                    idp = id + 1 as libc::c_int;
+                    igp = idp - nlm;
+                    ihp = iap - nlm;
+                    nlmp = nlm + 1 as libc::c_int;
+                    iip = ii + nlmp;
+                    x = exp(*fact.offset((iap - 1 as libc::c_int) as isize)
+                        + *fact.offset(ib as isize)
+                        + *fact.offset(ic as isize)
+                        + *fact.offset((idp - 1 as libc::c_int) as isize)
+                        - *fact.offset(ie as isize)
+                        - *fact.offset((nlmp - 1 as libc::c_int) as isize)
+                        - *fact.offset((igp - 1 as libc::c_int) as isize)
+                        - *fact.offset((ihp - 1 as libc::c_int) as isize)
+                        - *fact.offset((iip - 1 as libc::c_int) as isize));
+                    if r <= x {
+                        break;
+                    }
+                    sumprb = x;
+                    y = x;
+                    nll = nlm;
+                    lsp = 0 as libc::c_int;
+                    lsm = 0 as libc::c_int;
+                    while lsp == 0 {
+                        j = (id - nlm) * (ia - nlm);
+                        if j == 0 as libc::c_int {
+                            lsp = 1 as libc::c_int;
+                        } else {
+                            nlm = nlm + 1 as libc::c_int;
+                            x = x * j as libc::c_double / (nlm * (ii + nlm)) as libc::c_double;
+                            sumprb = sumprb + x;
+                            if r <= sumprb {
+                                done1 = 1 as libc::c_int;
+                                break;
+                            }
+                        }
+                        done2 = 0 as libc::c_int;
+                        while lsm == 0 {
+                            j = nll * (ii + nll);
+                            if j == 0 as libc::c_int {
+                                lsm = 1 as libc::c_int;
+                                break;
+                            } else {
+                                nll = nll - 1 as libc::c_int;
+                                y = y * j as libc::c_double
+                                    / ((id - nll) * (ia - nll)) as libc::c_double;
+                                sumprb = sumprb + y;
+                                if r <= sumprb {
+                                    nlm = nll;
+                                    done2 = 1 as libc::c_int;
+                                    break;
+                                } else if lsp == 0 {
+                                    break;
+                                }
+                            }
+                        }
+                        if done2 != 0 {
+                            break;
+                        }
+                    }
+                    if done1 != 0 {
+                        break;
+                    }
+                    if done2 != 0 {
+                        break;
+                    }
+                    r = r8_uniform_01(seed);
+                    r = sumprb * r;
+                }
+                *matrix.offset((l + m * nrow) as isize) = nlm;
+                ia = ia - nlm;
+                *jwork.offset(m as isize) = *jwork.offset(m as isize) - nlm;
+                m += 1;
+            }
+        }
+        *matrix.offset((l + (ncol - 1 as libc::c_int) * nrow) as isize) = ia;
+        l += 1;
+    }
+    j = 0 as libc::c_int;
+    while j < ncol - 1 as libc::c_int {
+        *matrix.offset((nrow - 1 as libc::c_int + j * nrow) as isize) = *jwork.offset(j as isize);
+        j += 1;
+    }
+    *matrix.offset((nrow - 1 as libc::c_int + (ncol - 1 as libc::c_int) * nrow) as isize) =
+        ib - *matrix.offset((nrow - 1 as libc::c_int + (ncol - 2 as libc::c_int) * nrow) as isize);
+    free(jwork as *mut libc::c_void);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use math::Quotient;
 use pyo3::prelude::*;
+use rand::Rng;
 use std::{cmp::min, vec};
 
+mod asa159;
 mod math;
 
 struct Pos(i64, i64);
@@ -153,10 +155,135 @@ pub fn exact(table: Vec<Vec<u64>>) -> PyResult<f64> {
     return Ok(p[0]);
 }
 
+#[pyfunction]
+pub fn sim(table: Vec<Vec<u32>>, iterations: u32) -> PyResult<f64> {
+    let mut row_sum: Vec<u32> = vec![];
+    let mut col_sum: Vec<u32> = vec![];
+
+    for i in 0..table.len() {
+        let mut temp = 0u32;
+        for j in 0..table[0].len() {
+            temp += table[i][j];
+        }
+        row_sum.push(temp);
+    }
+
+    for j in 0..table[0].len() {
+        let mut temp = 0;
+        for i in 0..table.len() {
+            temp += table[i][j];
+        }
+        col_sum.push(temp);
+    }
+
+    let mut rng = rand::thread_rng();
+
+    let mut results = vec![];
+
+    let mut n: usize = 0;
+    for sum in row_sum.clone() {
+        n += usize::try_from(sum).unwrap();
+    }
+
+    let mut fact = vec![0.0; n + 1];
+    fact[0] = 0.0;
+    fact[1] = 0.0;
+    let mut x = 2.0;
+    for i in 2..=n {
+        fact[i] = fact[i - 1] + f64::ln(x);
+        x += 1.0;
+    }
+
+    //dbg!(fact.clone());
+
+    let mut row_sum_i = vec![];
+    let mut col_sum_i = vec![];
+    for s in row_sum.clone() {
+        row_sum_i.push(s.try_into().unwrap());
+    }
+    for s in col_sum.clone() {
+        col_sum_i.push(s.try_into().unwrap());
+    }
+
+    for _ in 0..iterations {
+        let nrow = row_sum.len();
+        let ncol = col_sum.len();
+        unsafe {
+            let table = generate(&mut row_sum_i, &mut col_sum_i, rng.gen::<i32>());
+            //dbg!(table.clone());
+            // STATISTIC <- -sum(lfactorial(x))
+            let ans = find_statistic_c(table, nrow, ncol, &fact);
+            results.push(ans);
+        }
+    }
+
+    //dbg!(results.clone());
+
+    // PVAL <- (1 + sum(tmp <= STATISTIC/almost.1)) / (B + 1)
+
+    let mut sum = 0.0;
+    let statistic = find_statistic_r(table, &fact);
+
+    for i in 0..iterations {
+        if results[i as usize] <= statistic + 0.00000001 {
+            sum += 1.0;
+        }
+    }
+
+    let pvalue = (1.0 + sum) / (iterations as f64 + 1.0);
+
+    return Ok(pvalue);
+}
+
+fn find_statistic_c(table: Vec<i32>, nrow: usize, ncol: usize, fact: &Vec<f64>) -> f64 {
+    let mut ans = 0.0;
+    for i in 0..nrow {
+        for j in 0..ncol {
+            ans -= fact[table[i * ncol + j] as usize];
+        }
+    }
+    return ans;
+}
+
+fn find_statistic_r(table: Vec<Vec<u32>>, fact: &Vec<f64>) -> f64 {
+    let mut ans = 0.0;
+    for row in table {
+        for cell in row {
+            ans -= fact[cell as usize];
+        }
+    }
+    return ans;
+}
+
+unsafe fn generate(row_sum: &Vec<i32>, col_sum: &Vec<i32>, seed: i32) -> Vec<i32> {
+    let mut seed = [seed; 1];
+    let mut key = [0; 1];
+    let size = row_sum.len() * col_sum.len();
+    let mut matrix = vec![0; size];
+    let mut ierror = [0; 1];
+    asa159::rcont2(
+        i32::try_from(row_sum.len()).unwrap(),
+        i32::try_from(col_sum.len()).unwrap(),
+        row_sum.clone().as_mut_ptr(),
+        col_sum.clone().as_mut_ptr(),
+        key.as_mut_ptr(),
+        seed.as_mut_ptr(),
+        matrix.as_mut_ptr(),
+        ierror.as_mut_ptr(),
+    );
+    //dbg!(ierror);
+    //dbg!(matrix);
+    if ierror[0] != 0 {
+        panic!("Error generating contingency table ({})", ierror[0]);
+    }
+    return matrix;
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn fisher(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(exact, m)?)?;
+    m.add_function(wrap_pyfunction!(sim, m)?)?;
     Ok(())
 }
 
@@ -179,6 +306,7 @@ fn size4x4() {
     assert_eq!(output, 0.01096124432190799);
 }
 
+/*
 #[test]
 fn size5x4() {
     let input = vec![
@@ -189,4 +317,41 @@ fn size5x4() {
     ];
     let output = exact(input).unwrap();
     assert_eq!(output, 0.6388806191300838);
+}*/
+
+#[test]
+fn sim2x2() {
+    let input = vec![vec![3, 4], vec![4, 2]];
+    let result = sim(input.clone(), 10000).unwrap();
+    dbg!(result);
+    assert!(float_cmp::approx_eq!(f64, result, 0.592, epsilon = 0.02));
+    //sim(input.clone(), 12345, 1).unwrap();
+    //assert_eq!(output, 0.5920745920745921);
+}
+
+#[test]
+fn sim4x4() {
+    let input = vec![
+        vec![4, 1, 0, 1],
+        vec![1, 5, 0, 0],
+        vec![1, 1, 4, 2],
+        vec![1, 1, 0, 3],
+    ];
+    let result = sim(input.clone(), 10000).unwrap();
+    dbg!(result);
+    assert!(float_cmp::approx_eq!(f64, result, 0.011, epsilon = 0.004));
+}
+
+#[test]
+fn sim5x5() {
+    let input = vec![
+        vec![3, 1, 1, 1, 0],
+        vec![1, 4, 1, 0, 0],
+        vec![2, 1, 3, 2, 0],
+        vec![1, 1, 1, 2, 0],
+        vec![1, 1, 0, 0, 3],
+    ];
+    let result = sim(input, 10000).unwrap();
+    dbg!(result);
+    assert!(float_cmp::approx_eq!(f64, result, 0.222, epsilon = 0.02));
 }
