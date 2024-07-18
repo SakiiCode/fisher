4136f9196f2eb78f51b8cc9f521f6c185fc6de45
Sakii <13037299+SakiiCode@users.noreply.github.com>
2024-07-18 23:29:06 +0200

    disable optimizations for fexact only

--- build.rs
+++ build.rs
@@ -3,5 +3,8 @@ extern crate cc;
 fn main() {
     //println!(r"cargo:rustc-link-lib=static=f2c");
 
-    cc::Build::new().file("src/asa643.c").compile("fexact");
+    cc::Build::new()
+        .file("src/asa643.c")
+        .flag("-O0")
+        .compile("fexact");
 }
