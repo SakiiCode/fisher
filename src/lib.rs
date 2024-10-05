99a9446208584cf575900cbbc4dde52171f5b1fb
Sakii <13037299+SakiiCode@users.noreply.github.com>
2024-10-05 21:10:53 +0200

    move sim to another module

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,7 @@ use core::f64;
 use lazy_static::lazy_static;
 use math::Quotient;
 use pyo3::prelude::*;
-use rand::Rng;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use std::{cmp::min, sync::Mutex, vec};
+use std::{sync::Mutex, vec};
 use thread_local::ThreadLocal;
 
 use fixedsize::dfs;
@@ -19,111 +17,7 @@ mod asa159;
 mod asa643;
 mod fixedsize;
 mod math;
-
-macro_rules! get {
-    ($arr:ident, $r:expr, $c:expr, $cols:expr) => {
-        unsafe { *$arr.get_unchecked($r * $cols + $c) }
-    };
-}
-
-macro_rules! set {
-    ($arr:ident, $r:expr, $c:expr, $cols:expr, $val:expr) => {
-        unsafe { *$arr.get_unchecked_mut($r * $cols + $c) = $val }
-    };
-}
-fn fill(mat_new: &mut Vec<i32>, r_sum: &Vec<i32>, c_sum: &Vec<i32>, p_0: f64) -> f64 {
-    let r = r_sum.len();
-    let c = c_sum.len();
-    //print!("{:?} -> ", &mat_new);
-
-    for i in 0..r - 1 {
-        let mut temp = r_sum[i];
-        temp -= mat_new[i * c..(i + 1) * c].iter().sum::<i32>();
-        set!(mat_new, i, c - 1, c, temp);
-    }
-
-    let temp = c_sum[r - 1];
-    let mut sum = 0;
-
-    for j in (c - 1..mat_new.len()).step_by(c) {
-        sum += mat_new[j];
-    }
-    if temp < sum {
-        //println!();
-        return 0.0;
-    }
-
-    set!(mat_new, r - 1, c - 1, c, temp - sum);
-    //print!("{:?} ", &mat_new);
-
-    for j in 0..c - 1 {
-        let mut temp = c_sum[j];
-        for i in 0..r - 1 {
-            temp -= get!(mat_new, i, j, c);
-        }
-        set!(mat_new, r - 1, j, c, temp);
-    }
-
-    let n = r_sum.iter().sum::<i32>();
-
-    let mut p_1 = Quotient::new(n.try_into().unwrap(), &[], &[]);
-
-    p_1.mul_fact(&r_sum);
-    p_1.mul_fact(&c_sum);
-
-    p_1.div_fact(&[n; 1]);
-    p_1.div_fact(mat_new);
-
-    let p_1_res = p_1.solve();
-    if p_1_res <= p_0 {
-        //println!(" p={}", p_1_res);
-        return p_1_res;
-    } else {
-        //println!(" p=0.0");
-        return 0.0;
-    }
-}
-
-fn _dfs(
-    mat_new: &mut Vec<i32>,
-    xx: usize,
-    yy: usize,
-    r_sum: &Vec<i32>,
-    c_sum: &Vec<i32>,
-    p_0: f64,
-) -> f64 {
-    let r = r_sum.len();
-    let c = c_sum.len();
-
-    let mut max_2 = c_sum[yy];
-
-    let max_1 = r_sum[xx] - &mat_new[xx * c..(xx + 1) * c].iter().sum();
-
-    for index in (yy..mat_new.len()).step_by(c) {
-        max_2 -= mat_new[index];
-    }
-
-    let next_cycle = |k| {
-        let mut mat_new2 = mat_new.clone();
-        set!(mat_new2, xx, yy, c, k);
-        if xx + 2 == r && yy + 2 == c {
-            return fill(&mut mat_new2, r_sum, c_sum, p_0);
-        } else if xx + 2 == r {
-            return _dfs(&mut mat_new2, 0, yy + 1, r_sum, c_sum, p_0);
-        } else {
-            return _dfs(&mut mat_new2, xx + 1, yy, r_sum, c_sum, p_0);
-        }
-    };
-
-    if yy == 0 {
-        return (0..=min(max_1, max_2))
-            .into_par_iter()
-            .map(next_cycle)
-            .sum();
-    } else {
-        return (0..=min(max_1, max_2)).map(next_cycle).sum();
-    }
-}
+mod simulation;
 
 #[pyfunction]
 pub fn recursive(table: Vec<Vec<u32>>) -> PyResult<f64> {
@@ -270,46 +164,7 @@ pub fn fixed(table: Vec<Vec<i32>>) -> PyResult<f64> {
 
 #[pyfunction]
 pub fn sim(table: Vec<Vec<i32>>, iterations: i32) -> PyResult<f64> {
-    let row_sum: Vec<i32> = table.iter().map(|row| row.iter().sum()).collect();
-    let col_sum: Vec<i32> = (0..(table[0].len()))
-        .map(|index| table.iter().map(|row| row[index]).sum())
-        .collect();
-
-    let n = row_sum.iter().sum::<i32>().try_into().unwrap();
-
-    let mut fact = vec![0.0; n + 1];
-    fact[0] = 0.0;
-    let mut x = 1.0;
-    for i in 1..=n {
-        fact[i] = fact[i - 1] + f64::ln(x);
-        x += 1.0;
-    }
-
-    let row_sum_i = row_sum.iter().map(|s| (*s).try_into().unwrap()).collect();
-    let col_sum_i = col_sum.iter().map(|s| (*s).try_into().unwrap()).collect();
-
-    let nrow = row_sum.len();
-    let ncol = col_sum.len();
-    let statistic = find_statistic_r(&table, &fact) + f64::EPSILON;
-
-    let test = generate(&row_sum_i, &col_sum_i, &fact);
-    if let Err(error) = test {
-        println!("{}", error.1);
-        return Ok(-f64::from(error.0));
-    }
-
-    // STATISTIC <- -sum(lfactorial(x))
-    let sum = (0..iterations)
-        .into_par_iter()
-        .map(|_| generate(&row_sum_i, &col_sum_i, &fact))
-        .map(|table| find_statistic_c(&table.unwrap(), nrow, ncol, &fact))
-        .filter(|ans| *ans <= statistic)
-        .count() as f64;
-
-    // PVAL <- (1 + sum(tmp <= STATISTIC/almost.1)) / (B + 1)
-    let pvalue = (1.0 + sum) / (iterations as f64 + 1.0);
-
-    return Ok(pvalue);
+    Ok(simulation::calculate(table, iterations)?)
 }
 
 lazy_static! {
@@ -371,47 +226,6 @@ pub fn exact(table: Vec<Vec<i32>>, workspace: Option<i32>) -> PyResult<f64> {
     } else {
         return Ok(result);
     }
-}
-
-fn find_statistic_c(table: &Vec<i32>, nrow: usize, ncol: usize, fact: &Vec<f64>) -> f64 {
-    let mut ans = 0.0;
-    for i in 0..nrow {
-        for j in 0..ncol {
-            ans -= fact[table[i * ncol + j] as usize];
-        }
-    }
-    return ans;
-}
-
-fn find_statistic_r(table: &Vec<Vec<i32>>, fact: &Vec<f64>) -> f64 {
-    let mut ans = 0.0;
-    for row in table {
-        for cell in row {
-            ans -= fact[*cell as usize];
-        }
-    }
-    return ans;
-}
-
-fn generate(
-    row_sum: &Vec<i32>,
-    col_sum: &Vec<i32>,
-    fact: &Vec<f64>,
-) -> Result<Vec<i32>, (i32, &'static str)> {
-    let mut rng = rand::thread_rng();
-    let mut seed = rng.gen::<i32>();
-
-    let result = asa159::rcont2(
-        i32::try_from(row_sum.len()).unwrap(),
-        i32::try_from(col_sum.len()).unwrap(),
-        row_sum,
-        col_sum,
-        &mut 0,
-        &mut seed,
-        fact,
-    );
-
-    return result;
 }
 
 /// A Python module implemented in Rust.
@@ -736,84 +550,6 @@ fn proc16x8() {
     let result = exact(input, Some(200000000)).unwrap();
     dbg!(result);
     assert_eq!(result, -502.0);
-}
-
-#[test]
-fn sim2x2() {
-    let input = vec![vec![3, 4], vec![4, 2]];
-    let result = sim(input, 10000).unwrap();
-    dbg!(result);
-    assert!(float_cmp::approx_eq!(f64, result, 0.592, epsilon = 0.02));
-}
-
-#[test]
-fn sim4x4() {
-    let input = vec![
-        vec![4, 1, 0, 1],
-        vec![1, 5, 0, 0],
-        vec![1, 1, 4, 2],
-        vec![1, 1, 0, 3],
-    ];
-    let result = sim(input, 10000).unwrap();
-    dbg!(result);
-    assert!(float_cmp::approx_eq!(f64, result, 0.011, epsilon = 0.004));
-}
-
-#[test]
-fn sim4x4error() {
-    let input = vec![
-        vec![4, 1, 0, 1],
-        vec![1, 5, 0, 0],
-        vec![0, 0, 0, 0],
-        vec![1, 1, 0, 3],
-    ];
-    assert!(sim(input, 10000).unwrap() < 0.0);
-}
-
-#[test]
-fn sim4x4big() {
-    let input = vec![
-        vec![28, 28, 28, 0],
-        vec![0, 0, 0, 16],
-        vec![0, 0, 0, 5],
-        vec![0, 0, 0, 7],
-    ];
-    let result = sim(input, 100000).unwrap();
-    dbg!(result);
-    assert!(float_cmp::approx_eq!(f64, result, 0.0, epsilon = 0.004));
-}
-
-#[test]
-fn sim5x5() {
-    let input = vec![
-        vec![3, 1, 1, 1, 0],
-        vec![1, 4, 1, 0, 0],
-        vec![2, 1, 3, 2, 0],
-        vec![1, 1, 1, 2, 0],
-        vec![1, 1, 0, 0, 3],
-    ];
-    let result = sim(input, 10000).unwrap();
-    dbg!(result);
-    assert!(float_cmp::approx_eq!(f64, result, 0.222, epsilon = 0.02));
-}
-
-#[test]
-fn sim5x5_large() {
-    let input = vec![
-        vec![8, 8, 3, 5, 2],
-        vec![5, 3, 3, 0, 2],
-        vec![8, 9, 9, 0, 0],
-        vec![9, 4, 5, 3, 2],
-        vec![4, 6, 6, 1, 0],
-    ];
-    let result = sim(input, 10000000).unwrap();
-    dbg!(result);
-    assert!(float_cmp::approx_eq!(
-        f64,
-        result,
-        0.26314046636138944,
-        epsilon = 0.001
-    ));
 }
 
 #[test]
---
+++ src/simulation.rs
@@ -0,0 +1,169 @@
+use core::f64;
+use std::convert::Infallible;
+
+use rand::Rng;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
+use crate::asa159;
+
+pub fn calculate(table: Vec<Vec<i32>>, iterations: i32) -> Result<f64, Infallible> {
+    let row_sum: Vec<i32> = table.iter().map(|row| row.iter().sum()).collect();
+    let col_sum: Vec<i32> = (0..(table[0].len()))
+        .map(|index| table.iter().map(|row| row[index]).sum())
+        .collect();
+
+    let n = row_sum.iter().sum::<i32>().try_into().unwrap();
+
+    let mut fact = vec![0.0; n + 1];
+    fact[0] = 0.0;
+    let mut x = 1.0;
+    for i in 1..=n {
+        fact[i] = fact[i - 1] + f64::ln(x);
+        x += 1.0;
+    }
+
+    let row_sum_i = row_sum.iter().map(|s| (*s).try_into().unwrap()).collect();
+    let col_sum_i = col_sum.iter().map(|s| (*s).try_into().unwrap()).collect();
+
+    let nrow = row_sum.len();
+    let ncol = col_sum.len();
+    let statistic = find_statistic_r(&table, &fact) + f64::EPSILON;
+
+    let test = generate(&row_sum_i, &col_sum_i, &fact);
+    if let Err(error) = test {
+        println!("{}", error.1);
+        return Ok(-f64::from(error.0));
+    }
+
+    // STATISTIC <- -sum(lfactorial(x))
+    let sum = (0..iterations)
+        .into_par_iter()
+        .map(|_| generate(&row_sum_i, &col_sum_i, &fact))
+        .map(|table| find_statistic_c(&table.unwrap(), nrow, ncol, &fact))
+        .filter(|ans| *ans <= statistic)
+        .count() as f64;
+
+    // PVAL <- (1 + sum(tmp <= STATISTIC/almost.1)) / (B + 1)
+    let pvalue = (1.0 + sum) / (iterations as f64 + 1.0);
+
+    return Ok(pvalue);
+}
+
+fn find_statistic_c(table: &Vec<i32>, nrow: usize, ncol: usize, fact: &Vec<f64>) -> f64 {
+    let mut ans = 0.0;
+    for i in 0..nrow {
+        for j in 0..ncol {
+            ans -= fact[table[i * ncol + j] as usize];
+        }
+    }
+    return ans;
+}
+
+fn find_statistic_r(table: &Vec<Vec<i32>>, fact: &Vec<f64>) -> f64 {
+    let mut ans = 0.0;
+    for row in table {
+        for cell in row {
+            ans -= fact[*cell as usize];
+        }
+    }
+    return ans;
+}
+
+fn generate(
+    row_sum: &Vec<i32>,
+    col_sum: &Vec<i32>,
+    fact: &Vec<f64>,
+) -> Result<Vec<i32>, (i32, &'static str)> {
+    let mut rng = rand::thread_rng();
+    let mut seed = rng.gen::<i32>();
+
+    let result = asa159::rcont2(
+        i32::try_from(row_sum.len()).unwrap(),
+        i32::try_from(col_sum.len()).unwrap(),
+        row_sum,
+        col_sum,
+        &mut 0,
+        &mut seed,
+        fact,
+    );
+
+    return result;
+}
+
+#[test]
+fn sim2x2() {
+    let input = vec![vec![3, 4], vec![4, 2]];
+    let result = calculate(input, 10000).unwrap();
+    dbg!(result);
+    assert!(float_cmp::approx_eq!(f64, result, 0.592, epsilon = 0.02));
+}
+
+#[test]
+fn sim4x4() {
+    let input = vec![
+        vec![4, 1, 0, 1],
+        vec![1, 5, 0, 0],
+        vec![1, 1, 4, 2],
+        vec![1, 1, 0, 3],
+    ];
+    let result = calculate(input, 10000).unwrap();
+    dbg!(result);
+    assert!(float_cmp::approx_eq!(f64, result, 0.011, epsilon = 0.004));
+}
+
+#[test]
+fn sim4x4error() {
+    let input = vec![
+        vec![4, 1, 0, 1],
+        vec![1, 5, 0, 0],
+        vec![0, 0, 0, 0],
+        vec![1, 1, 0, 3],
+    ];
+    assert!(calculate(input, 10000).unwrap() < 0.0);
+}
+
+#[test]
+fn sim4x4big() {
+    let input = vec![
+        vec![28, 28, 28, 0],
+        vec![0, 0, 0, 16],
+        vec![0, 0, 0, 5],
+        vec![0, 0, 0, 7],
+    ];
+    let result = calculate(input, 100000).unwrap();
+    dbg!(result);
+    assert!(float_cmp::approx_eq!(f64, result, 0.0, epsilon = 0.004));
+}
+
+#[test]
+fn sim5x5() {
+    let input = vec![
+        vec![3, 1, 1, 1, 0],
+        vec![1, 4, 1, 0, 0],
+        vec![2, 1, 3, 2, 0],
+        vec![1, 1, 1, 2, 0],
+        vec![1, 1, 0, 0, 3],
+    ];
+    let result = calculate(input, 10000).unwrap();
+    dbg!(result);
+    assert!(float_cmp::approx_eq!(f64, result, 0.222, epsilon = 0.02));
+}
+
+#[test]
+fn sim5x5_large() {
+    let input = vec![
+        vec![8, 8, 3, 5, 2],
+        vec![5, 3, 3, 0, 2],
+        vec![8, 9, 9, 0, 0],
+        vec![9, 4, 5, 3, 2],
+        vec![4, 6, 6, 1, 0],
+    ];
+    let result = calculate(input, 10000000).unwrap();
+    dbg!(result);
+    assert!(float_cmp::approx_eq!(
+        f64,
+        result,
+        0.26314046636138944,
+        epsilon = 0.001
+    ));
+}
