6690d7d26f430a0bc2fc24a69198bf7e0a968dd0
SakiiCode <13037299+SakiiCode@users.noreply.github.com>
2026-04-11 20:57:20 +0200

    implement wide for 9x9

--- src/fixedsize.rs
+++ src/fixedsize.rs
@@ -1,7 +1,6 @@
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::{cell::RefCell, cmp::min, convert::Infallible, num::NonZero, ops::SubAssign};
 use thread_local::ThreadLocal;
-use wide::i32x4;
 
 use crate::math::Quotient;
 
@@ -11,18 +10,18 @@ macro_rules! set {
     };
 }
 
-type Simd = wide::i32x4;
-const N: usize = 4;
-
 // faster if not inlined
 #[inline(never)]
-fn fill(
-    mat_new: [i32; N * N],
-    r_sum: &[i32; N + 1],
-    c_sum: &[i32; N + 1],
+fn fill_4(
+    mat_new: [i32; 4 * 4],
+    r_sum: &[i32; 4 + 1],
+    c_sum: &[i32; 4 + 1],
     p_0: f64,
     tl: &ThreadLocal<Box<RefCell<Quotient>>>,
 ) -> f64 {
+    type Simd = wide::i32x4;
+    const N: usize = 4;
+
     //print!("{:?} -> ", &mat_new);
     let r_vec_red_arr: [i32; N] = (&c_sum[0..N]).try_into().unwrap();
     let mut r_vec_red: Simd = Simd::from(r_vec_red_arr);
@@ -82,19 +81,22 @@ fn fill(
 }
 
 pub fn dfs_4(
-    mat_new: [i32; N * N],
+    mat_new: [i32; 4 * 4],
     xx: usize,
     yy: usize,
-    r_sum: &[i32; N + 1],
-    c_sum: &[i32; N + 1],
+    r_sum: &[i32; 4 + 1],
+    c_sum: &[i32; 4 + 1],
     p_0: f64,
     tl: &ThreadLocal<Box<RefCell<Quotient>>>,
     threads: i32,
     max_threads: i32,
 ) -> f64 {
+    type Simd = wide::i32x4;
+    const N: usize = 4;
+
     let r = r_sum.len();
     let max_1_arr: [i32; N] = mat_new[(xx * N)..((xx + 1) * N)].try_into().unwrap();
-    let max_1 = r_sum[xx] - i32x4::new(max_1_arr).reduce_add();
+    let max_1 = r_sum[xx] - Simd::new(max_1_arr).reduce_add();
 
     let c = c_sum.len();
     let mut max_2 = c_sum[yy];
@@ -109,7 +111,7 @@ pub fn dfs_4(
         let mut mat_new2 = mat_new.clone();
         set!(mat_new2, xx, yy, c - 1, k);
         if xx + 2 == r && yy + 2 == c {
-            return fill(mat_new2, r_sum, c_sum, p_0, tl);
+            return fill_4(mat_new2, r_sum, c_sum, p_0, tl);
         }
 
         let (next_x, next_y) = next_cell(xx, yy, r, c);
@@ -130,6 +132,131 @@ pub fn dfs_4(
         return (0..=max).into_par_iter().map(next_cycle).sum();
     } else {
         return (0..=max).map(next_cycle).sum();
+    }
+}
+
+pub fn dfs_8(
+    mat_new: [i32; 8 * 8],
+    xx: usize,
+    yy: usize,
+    r_sum: &[i32; 8 + 1],
+    c_sum: &[i32; 8 + 1],
+    p_0: f64,
+    tl: &ThreadLocal<Box<RefCell<Quotient>>>,
+    threads: i32,
+    max_threads: i32,
+) -> f64 {
+    type Simd = wide::i32x8;
+    const N: usize = 8;
+
+    let r = r_sum.len();
+    let max_1_arr: [i32; N] = mat_new[(xx * N)..((xx + 1) * N)].try_into().unwrap();
+    let max_1 = r_sum[xx] - Simd::new(max_1_arr).reduce_add();
+
+    let c = c_sum.len();
+    let mut max_2 = c_sum[yy];
+
+    for index in (yy..N * N).step_by(N) {
+        max_2 -= mat_new[index]; //get!(mat_new, i, yy, c - 1);
+    }
+
+    let max = min(max_1, max_2);
+
+    let next_cycle = |k| {
+        let mut mat_new2 = mat_new.clone();
+        set!(mat_new2, xx, yy, c - 1, k);
+        if xx + 2 == r && yy + 2 == c {
+            return fill_8(mat_new2, r_sum, c_sum, p_0, tl);
+        }
+
+        let (next_x, next_y) = next_cell(xx, yy, r, c);
+        return dfs_8(
+            mat_new2,
+            next_x,
+            next_y,
+            r_sum,
+            c_sum,
+            p_0,
+            tl,
+            threads + max,
+            max_threads,
+        );
+    };
+
+    if threads < max_threads {
+        return (0..=max).into_par_iter().map(next_cycle).sum();
+    } else {
+        return (0..=max).map(next_cycle).sum();
+    }
+}
+
+// faster if not inlined
+#[inline(never)]
+fn fill_8(
+    mat_new: [i32; 8 * 8],
+    r_sum: &[i32; 8 + 1],
+    c_sum: &[i32; 8 + 1],
+    p_0: f64,
+    tl: &ThreadLocal<Box<RefCell<Quotient>>>,
+) -> f64 {
+    type Simd = wide::i32x8;
+    const N: usize = 8;
+
+    //print!("{:?} -> ", &mat_new);
+    let r_vec_red_arr: [i32; N] = (&c_sum[0..N]).try_into().unwrap();
+    let mut r_vec_red: Simd = Simd::from(r_vec_red_arr);
+
+    let mut r_vec = [Simd::from([0; N]); N];
+    for i in 0..N {
+        let start = i * N;
+        let arr: [i32; N] = (&mat_new[start..start + N]).try_into().unwrap();
+        r_vec[i] = Simd::from(arr);
+        r_vec_red.sub_assign(r_vec[i]);
+    }
+
+    let r_red_sum = r_vec_red.reduce_add();
+    let mut r_last = r_sum[N];
+
+    if r_last < r_red_sum {
+        //println!();
+        return 0.0;
+    }
+    r_last -= r_red_sum;
+
+    let c_vec_red_arr: [i32; N] = (&r_sum[0..N]).try_into().unwrap();
+    let mut c_vec_red: Simd = Simd::from(c_vec_red_arr);
+
+    let col = r_vec.map(|row| row.reduce_add());
+    let col_simd = Simd::from(col);
+    c_vec_red.sub_assign(col_simd);
+
+    let p_1_ref = tl.get_or(|| {
+        // r_sum is N+1 length, SIMD cannot be used
+        let n: i32 = r_sum.iter().sum();
+        let init_d = vec![n];
+        let mut init_n = Vec::with_capacity(2 * (N + 1) + 1);
+        init_n.extend_from_slice(r_sum);
+        init_n.extend_from_slice(c_sum);
+        Box::new(RefCell::new(Quotient::new(n as usize, &init_n, &init_d)))
+    });
+
+    let mut p_1 = (p_1_ref).borrow_mut();
+    p_1.clear();
+
+    p_1.div_fact(&mat_new);
+    p_1.div_fact(r_vec_red.as_array());
+
+    p_1.div_fact(c_vec_red.as_array());
+
+    p_1.div_fact(&[r_last]);
+
+    let p_1_res = p_1.solve();
+    if p_1_res <= p_0 {
+        //println!(" p={}", p_1_res);
+        p_1_res
+    } else {
+        //println!(" p=0.0");
+        0.0
     }
 }
 
@@ -192,8 +319,9 @@ pub fn calculate(table: Vec<Vec<i32>>) -> Result<f64, Infallible> {
             return Ok(-1.0);
         }
         2..=5 => 4,
+        6..=9 => 8,
         _ => {
-            println!("ERROR: fisher.recursive() can only be used with up to 5x5 matrices!");
+            println!("ERROR: fisher.recursive() can only be used with up to 9x9 matrices!");
             return Ok(-1.0);
         }
     };
@@ -209,21 +337,37 @@ pub fn calculate(table: Vec<Vec<i32>>) -> Result<f64, Infallible> {
         .unwrap_or(NonZero::new(12).unwrap())
         .get() as i32;
 
-    let row_sum_arr:[i32;5] = row_sum.try_into().unwrap();
-    let col_sum_arr:[i32;5] = col_sum.try_into().unwrap();
-
     let p = match lanes {
-        4 => dfs_4(
-            [0; 16],
-            0,
-            0,
-            &row_sum_arr,
-            &col_sum_arr,
-            stat,
-            &tl,
-            0,
-            max_threads,
-        ),
+        4 => {
+            let row_sum_arr: [i32; 5] = row_sum.try_into().unwrap();
+            let col_sum_arr: [i32; 5] = col_sum.try_into().unwrap();
+            dfs_4(
+                [0; 16],
+                0,
+                0,
+                &row_sum_arr,
+                &col_sum_arr,
+                stat,
+                &tl,
+                0,
+                max_threads,
+            )
+        }
+        8 => {
+            let row_sum_arr: [i32; 9] = row_sum.try_into().unwrap();
+            let col_sum_arr: [i32; 9] = col_sum.try_into().unwrap();
+            dfs_8(
+                [0; 64],
+                0,
+                0,
+                &row_sum_arr,
+                &col_sum_arr,
+                stat,
+                &tl,
+                0,
+                max_threads,
+            )
+        }
         _ => {
             unreachable!("Error in matching lane count. This should never happen");
         }
